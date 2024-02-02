9bb1b74093b24d24baba3f2ea1334395ee3c8d76
Harsh <harshmahajan2345@gmail.com>
2024-02-03 00:47:59 +0530

    done

---
+++ main.js
@@ -0,0 +1,53 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div>
+<div class="main">
+<h1>HARSH MAHAJAN <div class="roller">
+  <span id="rolltext">THIS<br/>
+  IS<br/>
+  MY PAGE<br/>
+  <span id="spare-time">long live the soviets</span><br/>
+  </div>
+  </h1>
+</div>
+<div class="container">
+<div class="col1">
+<div class="gallery">
+<img src="https://github.com/HarshMN2345/Portdesotiras/blob/main/src/images/soldier.jpg?raw=true" alt="a house on the mountain">
+<img src="https://picsum.photos/id/1054/300/300" alt="a big building">
+<img src="https://picsum.photos/id/101/300/300" alt="another house">
+<img src="https://picsum.photos/id/1065/300/300" alt="a small road between two houses">
+<img src="https://picsum.photos/id/1067/300/300" alt="a modern city">
+<img src="https://picsum.photos/id/188/300/300" alt="a lot of old houses">
+</div>
+</div>
+<div class="col2">
+<h1 id='ui'>SOFTWARE DEVELOPER</h1>
+</div>
+</div>
+<div class="contact">
+<div class="about-me-container">
+<div class="image-container">
+  <img src="https://cdn.wallpapersafari.com/27/48/Uz0SeD.jpg" alt="Profile Picture">
+</div>
+<div class="content">
+  <h1>About Me</h1>
+  <br>
+  <p>I'm Harsh Mahajan, a full-stack developer currently pursuing a BTech in CSE at MSIT(IPU). I specialize in web applications using Vanilla JavaScript as well as frameworks like React and Angular. I have experience in developing backends using ExpressJS and Django. My database skills include MySQL, MongoDB, and Firebase. My familiar languages include Java, HTML, CSS, C/C++/C#, JavaScript, TypeScript, Python, and I'm a beginner in Dart.</p>
+</div>
+</div>
+</div>
+<div class="contactreal">
+<div id="social-links">
+  <ul>
+    <li><a href="https://github.com/HarshMN2345/">GITHUB</a></li>
+    <li><a href="https://www.instagram.com/harshmahajan2345/">INSTAGRAM</i></a></li>
+    <li><a href="https://www.linkedin.com/in/harsh-mahajan-2345black/">LINKEDIN</i></a></li>
+  </ul>
+</div>
+</div>
+</div>
+`
+
+
